d601d1389022fe015956abe419e1bd9378545d90
Aleksey-Onishchenko <oddin16@gmail.com>
2020-02-26 16:53:23 +0200

    feat(gh-pages): method 'create' in progress

--- index.js
+++ index.js
@@ -1,36 +1,71 @@
+const shopProducts = [
+  {
+    id: 'adidas_jogger',
+    name: 'Addidas Jogger',
+    price: 100,
+    image: 'url.......'
+  },
+  {
+    id: 'adidas_samba',
+    name: 'Addidas Samba',
+    price: 120,
+    image: 'url.......'
+  },
+  {
+    id: 'nike_jordan',
+    name: 'Nike Air Jordan 1',
+    price: 200,
+    image: 'url.......'
+  },
+  {
+    id: 'nike_force',
+    name: 'Nike Air Force 1',
+    price: 100,
+    image: 'url.......'
+  }
+]
+
 class ShopProducts {
-  constructor() {
-     shopProducts = [
-      {
-        id: 'adidas_jogger',
-        name: 'Addidas Jogger',
-        price: 100,
-        image: 'url.......'
-      },
-      {
-        id: 'adidas_samba',
-        name: 'Addidas Samba',
-        price: 120,
-        image: 'url.......'
-      },
-      {
-        id: 'nike_jordan',
-        name: 'Nike Air Jordan 1',
-        price: 200,
-        image: 'url.......'
-      },
-      {
-        id: 'nike_force',
-        name: 'Nike Air Force 1',
-        price: 100,
-        image: 'url.......'
-      }
-    ]
+  constructor(config) {
+    this.shopProducts = config.shopProducts;
+  }
+
+
+  create() {
+   const newProduct = {
+    id: ,
+    name: ,
+    price: ,
+    image: ,
+   }
+   this.shopProducts.push(newProduct);
+  }
+
+
+  delete() {
+
+
+  }
+  
+  update() {
 
   }
 
-  
+  read() {
+
+  }
+
+
+
+  addProductToBusket() {
+
+  }
+
 }
+
+new ShopProducts({
+  images,
+});
 
 const ShopProductsInstance = new ShopProducts('root');
 
