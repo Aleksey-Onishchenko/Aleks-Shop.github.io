fe4d23b34f5a971828d422f3b6f3f661ac18877d
Aleksey-Onishchenko <oddin16@gmail.com>
2020-02-28 16:14:03 +0200

    gh-pages

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const shopProducts = [
   }
 ]
 
-ё1
+
 class ShopBasket {
   constructor(config) {
     this.shopProducts = config.shopProducts;
@@ -33,27 +33,64 @@ class ShopBasket {
   }
 
   create(id) {
-    const element = this.shopProducts.find(elem => elem.id === id);
+    // const element = this.shopProducts.find(elem => elem.id === id);
+    // const itemInBasket = this.shopBasket.find(item => item.id === id);
     
-    for (let i = 0; i < this.shopBasket.length; i += 1) {
-      const itemInBasket = this.shopBasket.find(item => item.id === id); // объект в корзине
+    // for (let i = 0; i < this.shopBasket.length; i += 1) {
 
-      if (itemInBasket === this.shopBasket[i]) {
-        console.log('we have 1 like this');
-        itemInBasket.quality += 1;
-        itemInBasket.total = itemInBasket.total * itemInBasket.quality;
-      } 
-    } if (element !== undefined) {
+    //   if (itemInBasket === this.shopBasket[i]) {
+    //     let startQuality = itemInBasket.total;
+    //     console.log('we have 1 like this');
+    //     itemInBasket.quality += 1;
+    //     itemInBasket.total = (startQuality * itemInBasket.quality);
+    //   } 
+    //   return;
+    // } 
+    // if (element !== undefined) {
+    //   const newProduct = {
+    //     id: id,
+    //     quality: 1,
+    //     total: element.price,
+    //   };
+    //   this.shopBasket.push(newProduct);
+    //   } else {
+    //     throw new Error('WRONG ID');
+    //   }
+
+
+    let element = this.shopProducts.find(elem => elem.id === id);
+    let itemInBasket = this.shopBasket.find(item => item.id === id);
+  
+    if (itemInBasket) {
+      console.log('нынишний элемент', itemInBasket);
+      let mainPrice = itemInBasket.total;
+      console.log('переменная mainPrice: ', mainPrice);
+      console.log('we already have this ID')
+      console.log('itemInBasket TOTAL', itemInBasket.total);
+      itemInBasket = {
+        ...itemInBasket,
+        id,
+        quality: itemInBasket.quality + 1,
+        total: mainPrice * itemInBasket.quality,
+      }
+      console.log('ARRAY', this.shopBasket)
+      console.log('next itemInBasket', itemInBasket);
+      return;
+    }
+    if (element !== undefined) {
       const newProduct = {
         id: id,
         quality: 1,
         total: element.price,
       };
       this.shopBasket.push(newProduct);
-    }
+      return;
+      }
+  
+    throw new Error('Wrond "ID"');
   }
-}
 
+}
 
 const ShopBasketInstance = new ShopBasket({
   shopProducts,
